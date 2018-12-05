d9a85e83a23a660cce3788ba9d9013d1c38ba5ec
Khine Myat Thwel <34429167+KhinePisi@users.noreply.github.com>
2018-12-05 16:59:05 +0630

    Add files via upload

---
+++ titanic_with_linear_regression.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+dataset=pd.read_csv("../input/titanic-dataset/train (1).csv")
+testset=pd.read_csv("../input/titanic-dataset/test.csv")
+gender_sub=pd.read_csv("../input/titanic/gender_submission.csv")
+trainedSurvived=dataset.iloc[:,:1].values
+trainedSex=dataset.iloc[:,1].values
+sexForPredict=testset.iloc[:,3:4].values
+testSurvived=gender_sub.iloc[:,1].values
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor=regressor.fit(trainedSurvived,trainedSex)
+predictions=np.around(regressor.predict(sexForPredict))
+from sklearn.metrics import accuracy_score
+accuracy_score(testSurvived,predictions)
+
+
