80141e25a509208ac61b5ab164f94d3c78dfaa18
Khine Myat Thwel <34429167+KhinePisi@users.noreply.github.com>
2018-12-05 19:16:06 +0630

    Update titanic_with_linear_regression.py

--- LinearRegression/titanic_with_linear_regression.py
+++ LinearRegression/titanic_with_linear_regression.py
@@ -1,17 +1,15 @@
 import pandas as pd
+import csv
 import numpy as np
-dataset=pd.read_csv("../input/-dataset/train (1).csv")
-testset=pd.read_csv("../input/-dataset/test.csv")
-gender_sub=pd.read_csv("../input//gender_submission.csv")
+dataset=pd.read_csv("../input/titanic-dataset/train (1).csv")
+testset=pd.read_csv("../input/titanic-dataset/test.csv")
 trainedSurvived=dataset.iloc[:,:1].values
 trainedSex=dataset.iloc[:,1].values
 sexForPredict=testset.iloc[:,3:4].values
-testSurvived=gender_sub.iloc[:,1].values
 from sklearn.linear_model import LinearRegression
 regressor=LinearRegression()
 regressor=regressor.fit(trainedSurvived,trainedSex)
 predictions=np.around(regressor.predict(sexForPredict))
-from sklearn.metrics import accuracy_score
-accuracy_score(testSurvived,predictions)
-
+data_to_submit = pd.DataFrame({'PassengerId':testset['PassengerId'],'Survived':predictions})
+data_to_submit.to_csv('titanicSubmission.csv', index = False)
 
