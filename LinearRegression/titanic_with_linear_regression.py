2369ce39bb7d6671624e17697167df8cc8e0d638
Khine Myat Thwel <34429167+KhinePisi@users.noreply.github.com>
2018-12-06 16:54:09 +0630

    Update titanic_with_linear_regression.py

--- LinearRegression/titanic_with_linear_regression.py
+++ LinearRegression/titanic_with_linear_regression.py
@@ -1,15 +1,16 @@
-import pandas as pd
-import csv
+#pandas for dataframes read and write
+#numpy for around function used here
+import pandas as pd 
 import numpy as np
-dataset=pd.read_csv("../input/titanic-dataset/train (1).csv")
+dataset=pd.read_csv("../input/titanic-dataset/train (1).csv") #the path here is according to Kernel in kaggle path . 
 testset=pd.read_csv("../input/titanic-dataset/test.csv")
-trainedSurvived=dataset.iloc[:,:1].values
-trainedSex=dataset.iloc[:,1].values
-sexForPredict=testset.iloc[:,3:4].values
-from sklearn.linear_model import LinearRegression
-regressor=LinearRegression()
-regressor=regressor.fit(trainedSurvived,trainedSex)
-predictions=np.around(regressor.predict(sexForPredict))
-data_to_submit = pd.DataFrame({'PassengerId':testset['PassengerId'],'Survived':predictions})
-data_to_submit.to_csv('titanicSubmission.csv', index = False)
+trainedSurvived=dataset.iloc[:,:1].values #get data values from survived column 
+trainedSex=dataset.iloc[:,1].values # get data values from Sex column
+sexForPredict=testset.iloc[:,3:4].values #data values of "Sex" from testset as we are gonna test whether a person will survive or not by gender .
+from sklearn.linear_model import LinearRegression #import LinearRegression from sklearn
+regressor=LinearRegression() #create model
+regressor=regressor.fit(trainedSurvived,trainedSex) #fit model with data values . Sex and survived.
+predictions=np.around(regressor.predict(sexForPredict)) #predict with testset , around() because the values are float but we need a definite , dead or alive result .
+data_to_submit = pd.DataFrame({'PassengerId':testset['PassengerId'],'Survived':predictions}) #save dataframe with output values
+data_to_submit.to_csv('titanicSubmission.csv', index = False)# final csv is produced.
 
